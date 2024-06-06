1818d5df0baedee8f1e9e9319bd0532bf3db47ae
mattsteen14 <mattsteen14@me.com>
2024-06-06 11:04:38 +0100

    get random quotes

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes', (req, res, next) => {
+    res.send(quotes);
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const quote = getRandomElement(quotes);
+    if (quote) {
+        res.send({quote});
+    } else {
+        res.status(404).send();
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
 });
