ceae6e59d4d29260e076a90d2477c2474374715a
mattsteen14 <mattsteen14@me.com>
2024-06-06 13:43:37 +0100

    refactored & separated into quotes.js/quotesRouter

--- server.js
+++ server.js
@@ -8,60 +8,8 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-// Get a certain requested author or all quotes
-app.get('/api/quotes', (req, res, next) => {
-    const person = req.query.person;
-    if (person) {
-        const personQuotes = quotes.filter(quote => quote.person === person);
-        res.send({ quotes: personQuotes })
-    } else {
-        res.send({ quotes });
-    }
-})
-
-// Get a random quote
-app.get('/api/quotes/random', (req, res, next) => {
-    const quote = getRandomElement(quotes);
-    if (quote) {
-        res.send({ quote });
-    } else {
-        res.status(404).send();
-    }
-})
-
-// Create a quote
-app.post('/api/quotes', (req, res, next) => {
-    const quote = req.query.quote;
-    const person = req.query.person;
-    if (quote && person) {
-        const newQuote = { id: quotes.length + 1, quote, person }; // Assign a unique ID
-        quotes.push(newQuote)
-        res.send({ quote: newQuote })
-    } else {
-        res.status(400).send();
-    }
-})
-
-// Update a quote
-app.put('/api/quotes/:id', (req, res, next) => {
-    const quoteIndex = getIndexById(req.params.id, quotes);
-    if (quoteIndex !== -1) {
-        const updatedQuote = updateElement(req.params.id, req.query, quotes);
-        res.send({ quote: updatedQuote })
-    } else {
-        res.status(404).send();
-    }
-})
-
-app.delete('/api/quotes/:id', (req, res, next) => {
-    const quoteIndex = getIndexById(req.params.id, quotes);
-    if (quoteIndex !== -1) {
-        quotes.splice(quoteIndex, 1);
-        res.status(204).send();
-    } else {
-        res.status(404).send();
-    }
-})
+const quotesRouter = require('./quotes');
+app.use('/api/quotes', quotesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
