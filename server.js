307e2fadd4050a94b444d9d856d16e86d349ac11
mattsteen14 <mattsteen14@me.com>
2024-06-06 11:54:34 +0100

    app.post

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-// Get all quotes
+// Get a certain requested author or all quotes
 app.get('/api/quotes', (req, res, next) => {
     const person = req.query.person;
     if (person) {
@@ -26,6 +26,18 @@ app.get('/api/quotes/random', (req, res, next) => {
         res.send({ quote });
     } else {
         res.status(404).send();
+    }
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    const quote = req.query.quote;
+    const person = req.query.person;
+    if (quote && person) {
+        const newQuote = { quote, person };
+        quotes.push(newQuote)
+        res.send({ quote: newQuote })
+    } else {
+        res.status(400).send();
     }
 })
 
